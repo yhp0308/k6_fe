e3544dd9a6586d916caf261f9f3d0fa68549dd19
yhp0308 <flash_hwan@naver.com>
2024-02-22 12:53:26 +0900

    JS_0222

---
+++ 03/03.js
@@ -0,0 +1,9 @@
+function handleClick(n) {
+
+    // 메세지 영역 가져오기
+    // document.getElementById("msgArea").innerHTML = "안녕하세요";
+    
+    // document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
+    document.querySelector("#msgArea").innerHTML =  `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
+
+    
