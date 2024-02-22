043cd9903b289ba6fbc70b02e6bce66aa6ce4fea
yhp0308 <flash_hwan@naver.com>
2024-02-22 12:59:37 +0900

    JS_0222

--- 03/03.js
+++ 03/03.js
@@ -1,9 +1,14 @@
-function handleClick(n) {
+/* function handleClick(n) {
 
     // 메세지 영역 가져오기
     // document.getElementById("msgArea").innerHTML = "안녕하세요";
     
     // document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
     document.querySelector("#msgArea").innerHTML =  `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
+    */
 
-    
+// 함수 작성 2번째 방법 : 화살표 함수
+
+const handleClick = (n) => {
+    document.querySelector("#msgArea").innerHTML =  `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
+}
